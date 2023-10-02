9c986a074e6eec25fa548b32d37b9b1d4ad5650c
Nathalia Da Rocha Santos <102376831+NathaliadaRocha07@users.noreply.github.com>
2023-10-01 23:31:34 -0300

    Update ExemploController.java

--- ado-desenvolvimento-web/ado-desenvolvimento-web/demo/src/main/java/com/teste/demo/ExemploController.java
+++ ado-desenvolvimento-web/ado-desenvolvimento-web/demo/src/main/java/com/teste/demo/ExemploController.java
@@ -18,7 +18,7 @@ public class ExemploController {
         mv.addObject("Email", "nathaliadarochasantos@gmail.com");
         mv.addObject("DataNasc", "Data de nascimento: 29/07/2002");
         mv.addObject("Github", "Github: https://github.com/NathaliadaRocha07");
-        mv.addObject("Tel", "11976013718");
+        mv.addObject("Tel", "11998017755");
         mv.addObject("Linkedin", "Linkedin: https://www.linkedin.com/in/nathalia-da-rocha-santos-19a052237/");
         mv.addObject("Curso", "Graduação em Análise e Desenvolvimento de Sistemas");
         mv.addObject("Habilidade", "Java, HTML, CSS, JS, MySQL");
